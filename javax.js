e4944f0f79b889cb31543bb61993f09555ba69cd
Lucas do Rosário Pereira <ldorosario@users.noreply.github.com>
2021-09-10 14:28:25 -0300

    Add files via upload

---
+++ javax.js
@@ -0,0 +1,6 @@
+
+function Chama_snack() {
+    var x = document.getElementById("snackbar"); 
+    x.className = "show";  
+    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+  } 
