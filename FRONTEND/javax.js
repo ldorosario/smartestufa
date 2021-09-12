d72cb61c6b46ebe20fcbc005d4d2708fec29bec0
Lucas do Rosário Pereira <ldorosario@users.noreply.github.com>
2021-09-12 10:47:39 -0300

    Delete FRONTEND directory

--- FRONTEND/javax.js
+++
@@ -1,6 +0,0 @@
-
-function Chama_snack() {
-    var x = document.getElementById("snackbar"); 
-    x.className = "show";  
-    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
-  } 
